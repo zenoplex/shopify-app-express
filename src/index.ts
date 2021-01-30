e4a30a0ac02e5384fae0b603393746d091d61114
Gen Ichino <zenoplex@gmail.com>
2021-01-30 18:13:41 +0900

    WIP

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import shopify from './routes/shopify';
 import morgan from 'morgan';
 
 const sessionSecret = process.env.SESSION_SECRET;
+const port = Number(process.env.PORT) || 3000;
 
 if (!sessionSecret) {
   throw new Error('SESSION_SECRET must be defined in env');
@@ -14,6 +15,8 @@ if (!sessionSecret) {
 
 const app = express();
 app.use(morgan('dev'));
+// https://github.com/expressjs/session#cookiesecure
+app.set('trust proxy', true);
 app.use(
   // Should use external store strategy in production
   session({
@@ -26,8 +29,6 @@ app.use(
 );
 app.use('/app', _app);
 app.use('/shopify', shopify);
-
-const port = Number(process.env.PORT) || 3000;
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
